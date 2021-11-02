9feab631b340c0f047d4ab52f7ea4582eace130c
Ryan Fulton <ryan.j.fulton@gmail.com>
2021-11-02 14:05:40 -0400

    Design changes to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,45 @@
 @extends('layouts.app')
 @section('content')
-    <body class="antialiased">
-                <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                    <div class="text-center">
 
-                        <img src="{{ asset('img/ryanfulton_logo.png') }}"/>
+<body class="antialiased">
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="text-center">
 
+            <img src="{{ asset('img/ryanfulton_logo.png') }}" />
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Welcome to my portfolio!') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+
+                        {{ __('Resume') }}
+                    </div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+
+                        {{ __('Projects') }}
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+</body>
 @endsection
